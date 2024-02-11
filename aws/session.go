fd667b0375e06df757d8dcb9a9d1ff28701770da
iriojose <iriojgomezv@gmail.com>
2024-02-11 17:57:18 -0400

    deploy to staging fist step

--- aws/session.go
+++ aws/session.go
@@ -1,13 +1,22 @@
 package aws
 
 import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 // configure your .aws/credentials file
 func NewSession() *session.Session {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
+	/* sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
+	})) */
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
 	}))
 
 	return sess
